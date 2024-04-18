e8d3b2c4d0aab69ee2cb5933c5236dd33469c594
Irving Alejandro Hernández Ramírez <ihernandezramirez800@gmail.com>
2024-04-17 20:54:36 -0600

    feat(): container, botones, fuente, iconos
    Co-Authored-By: Irving Alejandro Hernández Ramírez <160192346+axxving@users.noreply.github.com>

---
+++ src/components/App.jsx
@@ -0,0 +1,34 @@
+// Importando React
+import { Box, Button, Container, Typography } from "@mui/material";
+import React from "react";
+import AgricultureIcon from "@mui/icons-material/Agriculture";
+import AddModeratorIcon from '@mui/icons-material/AddModerator';
+
+export function App() {
+  return (
+    <Container className="Container" sx={{ boxShadow: 3, p: 5 }}>
+      <Typography variant="h1" textAlign="center">
+        Pelitoria
+      </Typography>
+      <Typography variant="h6" component="h2">
+        Encuentra todo lo que quieres ver.
+      </Typography>
+      <Typography variant="h4">
+        Encuentra una recomendacion cinematografica si es que estas indeciso
+        sobre que es lo que quieres ver hoy.
+      </Typography>
+      <Button startIcon={<AgricultureIcon />} color="success" variant="contained">
+        Mi primer boton
+      </Button>
+      <Button color="error" variant="contained">
+        Mi primer boton
+      </Button>
+      <Button color="primary" variant="outlined" endIcon={<AddModeratorIcon />}>
+        Mi primer boton
+      </Button>
+      <Box mt={3} sx={{ border: 2, p: 5 }}>
+        <Typography>Esta es una caja.</Typography>
+      </Box>
+    </Container>
+  );
+}
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,37 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { App } from "./components/App";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+
+// Estilos personalizados
+import "./assets/css/index.css";
+
+import { CssBaseline, ThemeProvider, createTheme } from "@mui/material";
+
+const theme = createTheme({
+  palette: {
+    mode: "light",
+    primary: {
+      main: "#1c2769",
+    },
+    secondary: {
+      main: "#f50057",
+    },
+    success: {
+      main: "#156019",
+    },
+  },
+});
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <App />
+    </ThemeProvider>
+  </React.StrictMode>
+);
